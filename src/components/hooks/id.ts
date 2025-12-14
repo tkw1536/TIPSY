25dc5320f0a2c7ea93171a55c0565d4e20101828
Tom Wiesing <tkw01536@gmail.com>
2025-12-14 11:15:21 +0100

    hooks: More documentation

--- src/components/hooks/id.ts
+++ src/components/hooks/id.ts
@@ -1,7 +1,16 @@
 import { useId } from 'preact/hooks'
 
-/** useOptionalID is like {@link useId}, but uses the user-supplied id if set */
+/**
+ * useOptionalID is like {@link useId}, but uses the user-supplied id if set.
+ */
 export function useOptionalId(id?: string): string {
+  // By the [rules of hooks], we need to call hooks at the top level.
+  // We are explicitly forbidden from calling it inside an if statement.
+  //
+  // This custom hook works around that restriction by
+  // always calling useId, but discarding its result if not needed.
+  //
+  // [rules of hooks]: https://react.dev/reference/rules/rules-of-hooks
   const theID = useId()
   return typeof id === 'string' ? id : theID
 }
